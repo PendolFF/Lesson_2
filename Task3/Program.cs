88c0f2b55b80477980bc37961bf0a1d6fec1e087
PendolFF <morev.ecrgo@gmail.com>
2024-03-19 18:28:47 +0700

    end of task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//Console.WriteLine("Enter array length:");
+//int arrlght = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i++;
+}
