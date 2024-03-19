71d16d6ce81ef7ae5530323fc94d37884aa1e00f
PendolFF <morev.ecrgo@gmail.com>
2024-03-19 17:23:08 +0700

    add chnges var for Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,10 +3,11 @@ int n = 10;
 int[] arr = new int[n];
 int i = 0;
 
-while (i < n)
+//while (i < n)
+while (i < arr.Length)
 {
     arr[i] = i + 1;
     // Console.Write(arr[i] + " "); 
-    Console.Write(${arr(i)} + " ");
+    Console.Write($"{arr[i]} ");
     i++;
 }
