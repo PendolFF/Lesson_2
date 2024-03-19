fc74d68d6b2650f40b08492d1ff2096940dfac73
PendolFF <morev.ecrgo@gmail.com>
2024-03-19 19:26:03 +0700

    End of my work task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// мое решение
+int[] array = { 1, 10, 3, 4, 5 };
+int i = 0;
+int max = array[0];
+
+while (i < array.Length)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i++;
+}
+Console.WriteLine(max);
