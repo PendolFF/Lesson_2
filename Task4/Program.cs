aab285b8cf745082c83adc90d4206776635150bc
PendolFF <morev.ecrgo@gmail.com>
2024-03-19 19:37:39 +0700

    add new text FOR in tsak4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,14 +1,28 @@
 ﻿// мое решение
-int[] array = { 1, 10, 3, 4, 5 };
-int i = 0;
-int max = array[0];
+// int[] array = { 1, 10, 3, 4, 5 };
+//int i = 0;
+//int max = array[0];
 
-while (i < array.Length)
+//while (i < array.Length)
+//{
+//  if (array[i] > max)
+//  {
+//      max = array[i];
+//  }
+//  i++;
+// }
+//Console.WriteLine(max);
+
+// вариант через FOR используется при известном заранее числе итераций
+
+
+int[] array = { 1, 10, 3, 24, 5 };
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
 {
     if (array[i] > max)
     {
         max = array[i];
     }
-    i++;
 }
 Console.WriteLine(max);
